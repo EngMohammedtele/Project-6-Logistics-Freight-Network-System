e3002c88fc74bc537b5ca82f6eb9746ef879c45a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:38 +0400

    docs(carrier): document carrier repository

--- src/main/java/com/codelegends/logistics/repository/CarrierRepository.java
+++ src/main/java/com/codelegends/logistics/repository/CarrierRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Carrier;
 
+/**
+ * Provides active-record persistence operations for carrier entities.
+ */
 public interface CarrierRepository extends ActiveRepository<Carrier> {}
