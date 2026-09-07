94b06f62984ed51f07d4dc9abf7f5d13d74d10da
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:27:44 +0400

    docs(route): document planned route state

--- src/main/java/com/codelegends/logistics/entity/RouteStatus.java
+++ src/main/java/com/codelegends/logistics/entity/RouteStatus.java
@@ -4,6 +4,7 @@ package com.codelegends.logistics.entity;
  * Tracks the lifecycle state of a delivery route.
  */
 public enum RouteStatus {
+    // Indicates the route has been created but dispatch has not started.
     PLANNED,
     IN_PROGRESS,
     COMPLETED
