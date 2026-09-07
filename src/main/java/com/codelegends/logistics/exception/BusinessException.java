66d0ac0a9f0deb0c9027a1b54587761b00f1eb1a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:37 +0400

    docs(exception): document business exception role

--- src/main/java/com/codelegends/logistics/exception/BusinessException.java
+++ src/main/java/com/codelegends/logistics/exception/BusinessException.java
@@ -1,5 +1,8 @@
 package com.codelegends.logistics.exception;
 
+/**
+ * Signals business-rule violations that should be returned as client errors.
+ */
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
