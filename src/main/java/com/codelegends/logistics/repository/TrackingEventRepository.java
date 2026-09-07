1b665ce07dfc87c94e60cdcdaeb4664c16c5fe88
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:40 +0400

    docs(tracking): document tracking event repository

--- src/main/java/com/codelegends/logistics/repository/TrackingEventRepository.java
+++ src/main/java/com/codelegends/logistics/repository/TrackingEventRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.TrackingEvent;
 
+/**
+ * Provides active-record persistence operations for trackingevent entities.
+ */
 public interface TrackingEventRepository extends ActiveRepository<TrackingEvent> {}
