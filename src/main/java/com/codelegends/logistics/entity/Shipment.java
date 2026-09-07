747a0c5c65d15799d081a80ef98e351137d15719
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:27:37 +0400

    docs(shipment): clarify customer shipment setters

--- src/main/java/com/codelegends/logistics/entity/Shipment.java
+++ src/main/java/com/codelegends/logistics/entity/Shipment.java
@@ -17,6 +17,7 @@ import java.util.*;
 @Table(name = "shipment")
 // Generates read accessors for persisted customer shipment properties.
 @Getter
+// Generates write accessors used by JPA and service mapping for customer shipment.
 @Setter
 @NoArgsConstructor
 public class Shipment extends BaseClass {
