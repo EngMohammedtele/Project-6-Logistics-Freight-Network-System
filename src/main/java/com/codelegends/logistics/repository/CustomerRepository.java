f94ee5798e74db3d1aad0c84e0e416d06f41dda4
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:39 +0400

    docs(customer): document customer repository

--- src/main/java/com/codelegends/logistics/repository/CustomerRepository.java
+++ src/main/java/com/codelegends/logistics/repository/CustomerRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Customer;
 
+/**
+ * Provides active-record persistence operations for customer entities.
+ */
 public interface CustomerRepository extends ActiveRepository<Customer> {}
