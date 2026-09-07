819731cda6829d1b3f0e5165e36205c1e862b914
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:40 +0400

    docs(service-zone): document service zone repository

--- src/main/java/com/codelegends/logistics/repository/ServiceZoneRepository.java
+++ src/main/java/com/codelegends/logistics/repository/ServiceZoneRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.ServiceZone;
 
+/**
+ * Provides active-record persistence operations for servicezone entities.
+ */
 public interface ServiceZoneRepository extends ActiveRepository<ServiceZone> {}
