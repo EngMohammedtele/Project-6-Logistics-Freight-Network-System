b0fa49c7837425bd641ab24d1eb6c4a2a0ed6f2c
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:27:45 +0400

    docs(vehicle): document van vehicle type

--- src/main/java/com/codelegends/logistics/entity/VehicleType.java
+++ src/main/java/com/codelegends/logistics/entity/VehicleType.java
@@ -4,6 +4,7 @@ package com.codelegends.logistics.entity;
  * Defines the supported vehicle categories for carrier fleets.
  */
 public enum VehicleType {
+    // Represents a smaller fleet vehicle used for lighter deliveries.
     VAN,
     TRUCK
 }
