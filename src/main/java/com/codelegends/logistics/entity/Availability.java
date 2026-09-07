4438bfeea691ef0a9c5bb35169bdad508bd31541
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:27:44 +0400

    docs(driver): document available assignment state

--- src/main/java/com/codelegends/logistics/entity/Availability.java
+++ src/main/java/com/codelegends/logistics/entity/Availability.java
@@ -4,6 +4,7 @@ package com.codelegends.logistics.entity;
  * Defines whether a driver or vehicle can currently be assigned to route work.
  */
 public enum Availability {
+    // Indicates the resource can be assigned to route work.
     AVAILABLE,
     ASSIGNED,
     MAINTENANCE
