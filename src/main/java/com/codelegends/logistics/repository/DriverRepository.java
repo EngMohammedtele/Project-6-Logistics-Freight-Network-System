251372bf861546897541c9b2697ce866f9f9be8d
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:39 +0400

    docs(driver): document driver repository

--- src/main/java/com/codelegends/logistics/repository/DriverRepository.java
+++ src/main/java/com/codelegends/logistics/repository/DriverRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Driver;
 
+/**
+ * Provides active-record persistence operations for driver entities.
+ */
 public interface DriverRepository extends ActiveRepository<Driver> {}
