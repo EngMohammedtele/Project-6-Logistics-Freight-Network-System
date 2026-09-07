7885d03a8b2e339bad695da6de66e5d738f56373
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:27:44 +0400

    docs(customer): document individual customer type

--- src/main/java/com/codelegends/logistics/entity/CustomerType.java
+++ src/main/java/com/codelegends/logistics/entity/CustomerType.java
@@ -4,6 +4,7 @@ package com.codelegends.logistics.entity;
  * Classifies customers as individual or business accounts.
  */
 public enum CustomerType {
+    // Identifies a personal customer account.
     INDIVIDUAL,
     BUSINESS
 }
