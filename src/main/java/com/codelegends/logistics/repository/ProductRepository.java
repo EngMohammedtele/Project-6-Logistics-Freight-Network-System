003be026e81f49173a3179e0148326ee64a470a7
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:39 +0400

    docs(product): document product repository

--- src/main/java/com/codelegends/logistics/repository/ProductRepository.java
+++ src/main/java/com/codelegends/logistics/repository/ProductRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Product;
 
+/**
+ * Provides active-record persistence operations for product entities.
+ */
 public interface ProductRepository extends ActiveRepository<Product> {}
