3cadae3ac1dc1c3175e1283b086204b9d90169c1
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:38 +0400

    docs(app): document application bootstrap entry point

--- src/main/java/com/codelegends/logistics/LogisticsApplication.java
+++ src/main/java/com/codelegends/logistics/LogisticsApplication.java
@@ -3,6 +3,9 @@ package com.codelegends.logistics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Bootstraps the Logistics Freight Network Spring Boot application.
+ */
 @SpringBootApplication
 public class LogisticsApplication {
     public static void main(String[] args) {
