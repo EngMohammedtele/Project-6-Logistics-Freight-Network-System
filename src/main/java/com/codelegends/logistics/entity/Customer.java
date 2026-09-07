a6444eb25248cead18b968c1b793dc22d3467099
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:27:41 +0400

    docs(customer): document customer account jpa constructor

--- src/main/java/com/codelegends/logistics/entity/Customer.java
+++ src/main/java/com/codelegends/logistics/entity/Customer.java
@@ -18,6 +18,7 @@ import java.util.*;
 @Getter
 // Generates write accessors used by JPA and service mapping for customer account.
 @Setter
+// Provides the default constructor required by JPA for customer account hydration.
 @NoArgsConstructor
 public class Customer extends BaseClass {
 
