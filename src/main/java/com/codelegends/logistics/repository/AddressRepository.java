53ec7cafe63cce28e023aec725d72e6337bceaa2
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:38 +0400

    docs(address): document address repository

--- src/main/java/com/codelegends/logistics/repository/AddressRepository.java
+++ src/main/java/com/codelegends/logistics/repository/AddressRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Address;
 
+/**
+ * Provides active-record persistence operations for address entities.
+ */
 public interface AddressRepository extends ActiveRepository<Address> {}
