dbec1081a93ad5799799c4a8b479b9cfa6d6b0d9
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:40 +0400

    docs(staff): document staff repository

--- src/main/java/com/codelegends/logistics/repository/StaffRepository.java
+++ src/main/java/com/codelegends/logistics/repository/StaffRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Staff;
 
+/**
+ * Provides active-record persistence operations for staff entities.
+ */
 public interface StaffRepository extends ActiveRepository<Staff> {}
