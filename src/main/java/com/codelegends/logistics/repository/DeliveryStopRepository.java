934b57bb9a97a066878e9f5f3e2531784e456b2a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:39 +0400

    docs(delivery): document delivery stop repository

--- src/main/java/com/codelegends/logistics/repository/DeliveryStopRepository.java
+++ src/main/java/com/codelegends/logistics/repository/DeliveryStopRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.DeliveryStop;
 
+/**
+ * Provides active-record persistence operations for deliverystop entities.
+ */
 public interface DeliveryStopRepository extends ActiveRepository<DeliveryStop> {}
