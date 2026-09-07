b790122f7915b0a757516b389628a4eca038efcb
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 15:49:15 +0400

    docs(customer): document address customer resolution

--- src/main/java/com/codelegends/logistics/service/AddressService.java
+++ src/main/java/com/codelegends/logistics/service/AddressService.java
@@ -45,6 +45,7 @@ public class AddressService extends CrudService<Address, AddressDTO> {
         entity.setPostalCode(dto.getPostalCode());
         // Copies the country value onto the address entity.
         entity.setCountry(dto.getCountry());
+        // Resolves the owning customer before attaching it to the address.
         entity.setCustomer(access.get(Customer.class, dto.getCustomerId()));
         entity.setServiceZone(access.get(ServiceZone.class, dto.getServiceZoneId()));
     }
