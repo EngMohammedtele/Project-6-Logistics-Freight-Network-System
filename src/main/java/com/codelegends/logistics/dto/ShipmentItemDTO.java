663bbcc06dd785a7fa3b4d95bb5457c353f0f730
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:10:42 +0400

    docs(shipment): clarify shipment item DTO description

--- src/main/java/com/codelegends/logistics/dto/ShipmentItemDTO.java
+++ src/main/java/com/codelegends/logistics/dto/ShipmentItemDTO.java
@@ -11,7 +11,7 @@ import java.time.*;
 import java.util.List;
 
 /**
- * Transfers shipmentitem data between API clients and the service layer.
+ * Transfers shipment item data between API clients and the service layer.
  */
 @Data
 @Builder
