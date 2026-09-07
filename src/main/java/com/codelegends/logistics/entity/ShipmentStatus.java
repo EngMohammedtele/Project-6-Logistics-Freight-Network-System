37529766438a70f3525e713a45a45b22f48eb495
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:01:08 +0400

    docs(shipment): document shipment status enum

--- src/main/java/com/codelegends/logistics/entity/ShipmentStatus.java
+++ src/main/java/com/codelegends/logistics/entity/ShipmentStatus.java
@@ -1,5 +1,8 @@
 package com.codelegends.logistics.entity;
 
+/**
+ * Tracks the lifecycle state of a shipment as tracking events are recorded.
+ */
 public enum ShipmentStatus {
     CREATED,
     PICKED_UP,
