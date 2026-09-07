5bc3b8fa37427b968f48f7eef723c1078ba5b20f
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:40 +0400

    docs(shipment): document shipment item repository

--- src/main/java/com/codelegends/logistics/repository/ShipmentItemRepository.java
+++ src/main/java/com/codelegends/logistics/repository/ShipmentItemRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.ShipmentItem;
 
+/**
+ * Provides active-record persistence operations for shipmentitem entities.
+ */
 public interface ShipmentItemRepository extends ActiveRepository<ShipmentItem> {}
