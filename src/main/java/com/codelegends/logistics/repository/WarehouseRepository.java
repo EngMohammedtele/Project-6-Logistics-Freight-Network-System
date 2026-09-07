8d39b6f67e0f494d4f4a82c602ce34902eeec85a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-07 11:03:40 +0400

    docs(warehouse): document warehouse repository

--- src/main/java/com/codelegends/logistics/repository/WarehouseRepository.java
+++ src/main/java/com/codelegends/logistics/repository/WarehouseRepository.java
@@ -2,4 +2,7 @@ package com.codelegends.logistics.repository;
 
 import com.codelegends.logistics.entity.Warehouse;
 
+/**
+ * Provides active-record persistence operations for warehouse entities.
+ */
 public interface WarehouseRepository extends ActiveRepository<Warehouse> {}
